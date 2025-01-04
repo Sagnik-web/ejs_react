63603ce3d40a113f686bb210b6211e85390c8598
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2025-01-04 14:54:00 +0530

    updt ejs & react

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client'
 
-const App = () => <h1>Hello, EJS and React!</h1>;
+const App = () => <h1>Hello, EJS and React My Self Sagnik Biswas!</h1>;
 
 createRoot(document.getElementById('root')).render(<App />)
   
